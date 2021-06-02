c65260d480f7bc6b806aa6b628757b869e5bbac3
Alberto Costa de Araújo <albertobsb@outlook.com>
2021-06-01 21:19:14 -0300

    Projeto base fazendo exception

---
+++ NotiticationApi/Controllers/CostumerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotiticationApi.Models;
+
+namespace NotiticationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostumerController : ControllerBase
+    {
+        [HttpPost]
+        public void Post(Customer customer)
+        {
+            var c = new Customer("", "");
+        }
+    }
+}
---
+++ NotiticationApi/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NotiticationApi.Models
+{
+    public class Customer
+    {
+        public string Name { get; }
+        public string Email { get; }
+
+        //To swagger
+        public Customer()
+        {}
+
+        public Customer(string name, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new InvalidOperationException("Name cannot be empty");
+            }
+            if (!email.Contains("@"))
+            {
+                throw new InvalidOperationException("Invalid email");
+            }
+        }
+    }
+}
